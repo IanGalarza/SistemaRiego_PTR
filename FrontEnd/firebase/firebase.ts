9e06f8a3bf3a555644093fa93ec9ea06e4f30f39
Ian Galarza <132719915+IanGalarza@users.noreply.github.com>
2025-12-05 20:42:07 -0300

    hide

--- FrontEnd/firebase/firebase.ts
+++ FrontEnd/firebase/firebase.ts
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAeE7mYJke8to857s5n75143OKUreuTv_w",
+    apiKey: "******",
     authDomain: "sistemariegoptr.firebaseapp.com",
     projectId: "sistemariegoptr",
     storageBucket: "sistemariegoptr.firebasestorage.app",
-    messagingSenderId: "759970982954",
-    appId: "1:759970982954:web:c97837b1878ac19ce7a753"
+    messagingSenderId: "*******",
+    appId: "*******"
 };
 
 
